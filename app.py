9f5c63e03c5f5ad56d7b9fe6e2e6fa0443fe5dca
Ernesto Vilela <ernesto.vilela@ucb.edu.bo>
2022-12-07 16:46:41 -0400

    Adding clasificators

--- app.py
+++ app.py
@@ -13,4 +13,33 @@ def predict():
     classifier = joblib.load('classifier.pkl')
     prediction = classifier.predict(query)
     return jsonify({'prediction': int(prediction)})
+
+
+@app.route('/naive-bayes', methods=['POST'])
+def naiveBayes():
+    json = request.get_json()
+    query_df = pd.DataFrame(json)
+    query = pd.get_dummies(query_df)
+    classifier = joblib.load('naive_bayes.pkl')
+    prediction = classifier.predict(query)
+    return jsonify({'prediction': int(prediction)})    
     
+
+@app.route('/naive-bayes', methods=['POST'])
+def randomForest():
+    json = request.get_json()
+    query_df = pd.DataFrame(json)
+    query = pd.get_dummies(query_df)
+    classifier = joblib.load('random_forest.pkl')
+    prediction = classifier.predict(query)
+    return jsonify({'prediction': int(prediction)})    
+
+
+@app.route('/logistic-regresion', methods=['POST'])
+def randomForest():
+    json = request.get_json()
+    query_df = pd.DataFrame(json)
+    query = pd.get_dummies(query_df)
+    classifier = joblib.load('logistic_regresion.pkl')
+    prediction = classifier.predict(query)
+    return jsonify({'prediction': int(prediction)})    
